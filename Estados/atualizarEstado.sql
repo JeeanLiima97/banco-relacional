d9040bd767b6ccc610ce37c49e4c8bf033573f27
Jean Lima <jean.jr.souza@hotmail.com>
2019-11-28 19:03:10 -0300

    Adicionado metodo InserirEstadosTeste

--- Estados/atualizarEstado.sql
+++ Estados/atualizarEstado.sql
@@ -1,3 +1,16 @@
 update estados
 set nome = 'Maranhão' 
 where sigla= 'MA';
+
+select nome
+from estados
+where sigla = 'MA';
+
+
+update estados
+set nome = 'Paraná', populacao = 11.40
+where nome = 'Parana';
+
+select nome, sigla
+from estados
+where nome = 'Paraná';
